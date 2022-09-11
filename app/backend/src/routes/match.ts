b37ae734674c201243c2a9ec255706ab53415f27
pablovr1000 <pablovr200@hotmail.com>
2022-09-11 20:52:24 -0300

    chore: Adcionado comentários e match para editar posteriormente

--- app/backend/src/routes/match.ts
+++ app/backend/src/routes/match.ts
@@ -8,8 +8,8 @@ const matchesController = new MatchesController();
 
 routerMatch.get('/matches', matchesController.getAllMatches);
 routerMatch.get('/matches/:id', matchesController.getMatchById);
-routerMatch.post('/matches', validateToken, matchesController.createMatch);
-routerMatch.patch('/matches/:id', matchesController.updateMatch);
-routerMatch.patch('/matches/:id/finish', matchesController.finishMatch);
+routerMatch.post('/matches', validateToken, matchesController.createMatch); // Add validação Joi
+routerMatch.patch('/matches/:id', validateToken, matchesController.updateMatch); // Add validação Joi
+routerMatch.patch('/matches/:id/finish', validateToken, matchesController.finishMatch);
 
 export default routerMatch;
