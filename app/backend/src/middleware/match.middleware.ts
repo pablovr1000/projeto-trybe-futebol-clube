d2721da39e9cc6f4d82780e7927bd51a45bd95a1
pablovr1000 <pablovr200@hotmail.com>
2022-09-12 04:31:01 -0300

    fix: ajustes em matches

--- app/backend/src/middleware/match.middleware.ts
+++ app/backend/src/middleware/match.middleware.ts
@@ -1,24 +1,19 @@
 import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import Team from '../database/models/Teams';
-import StatusError from '../utils/http.exception';
 
-const createMatchValidation = async (request: Request, _response: Response, next: NextFunction) => {
+const createMatchValidation = async (request: Request, response: Response, next: NextFunction) => {
   const { homeTeam, awayTeam } = request.body;
   if (homeTeam === awayTeam) {
-    throw new StatusError(
-      'It is not possible to create a match with two equal teams',
-      StatusCodes.UNAUTHORIZED,
-    );
+    return response.status(StatusCodes.UNAUTHORIZED)
+      .json({ message: 'It is not possible to create a match with two equal teams' });
   }
 
   const homeTeamIsExistent = await Team.findByPk(homeTeam);
   const awayTeamIsExistent = await Team.findByPk(awayTeam);
   if (!homeTeamIsExistent || !awayTeamIsExistent) {
-    throw new StatusError(
-      'There is no team with such id!',
-      StatusCodes.NOT_FOUND,
-    );
+    return response.status(StatusCodes.NOT_FOUND)
+      .json({ message: 'There is no team with such id!' });
   }
 
   next();
